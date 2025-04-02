aaa5c9d999a2e57ac5bd8ebeaa1c76b1aa8fcfe3
berkin9 <berkinakbiyik@hotmail.com>
2025-04-02 14:52:11 +0300

    packages and comments

--- 2.Routing/12-middleware/middleware.js
+++ 2.Routing/12-middleware/middleware.js
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 
 const cors = {
-    origins: ["https://www.tanersaydan.net", "https://www.blabla.com"],
-    methods: ["POST","PUT"],
-    headers: ["Authorizations"]
+    origins: ["https://www.blabla.com"],//allowed websites
+    methods: ["POST","PUT"],//allowed request methods
+    headers: ["Authorizations"]//allowed headers
 }
 
-export function middleware(request){
+export function middleware(request){//request param
     const origin = request.headers.get("origin") ?? "";
     const isAllowedOrigin = cors.origins.includes(origin); 
     const isAllowedMethod = cors.methods.includes(request.method);
@@ -21,12 +21,13 @@ export function middleware(request){
             {headers: {"Access-Control-Allow-Methods": "POST,PUT"} });
     }
     
-    const response = NextResponse.next();
+    const response = NextResponse.next();//get response
+	//response.cookies.set();
 
     return response;
 }
 
 
 export const config = {
-    matcher: '/api/:path*',
+    matcher: '/api/:path*',//matcher indicates where to mw run
   }
